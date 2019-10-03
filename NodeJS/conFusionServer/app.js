a7f855f279d7da133979d8f11beb24cfd93cec09
BhavanaVanjani <bhavana.vanjani@gmail.com>
2019-10-03 15:57:07 +0530

    File Upload

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -14,7 +14,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-
+var uploadRouter = require('./routes/uploadRouter');
 const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
@@ -57,6 +57,7 @@ app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 
 app.use(express.static(path.join(__dirname, 'public')));
