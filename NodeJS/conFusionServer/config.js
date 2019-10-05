f9528e42284c7e660f407a5bdd6babcc8f8a2679
BhavanaVanjani <bhavana.vanjani@gmail.com>
2019-10-05 19:36:20 +0530

    Passport Facebook

--- NodeJS/conFusionServer/config.js
+++ NodeJS/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27015/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27015/conFusion',
+    'facebook' : {
+        clientId: '3391628040847409',
+        clientSecret: '8afa1414ead026d888b7d525b29cd3bc'
+    }
 }
